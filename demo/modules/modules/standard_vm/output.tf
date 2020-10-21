f32b92eeb761f254420ec34042d91e209b2e60a9
Jamie Jonas <jamie@oneten.tech>
2020-10-21 13:54:56 +0100

    added notes

--- demo/modules/modules/standard_vm/output.tf
+++ demo/modules/modules/standard_vm/output.tf
@@ -1,11 +1,13 @@
-# output "vm_name" {
-#     value = zipmap(
-#         [for v in azurerm_virtual_machine.standard : v.name],
-#         [for p in random_password.password : p.result])
-# }
+# The for expresstion below is detailed here
+# azurerm_virtual_machine.standard has multiple objects and we need an expression to combine the password and vmname
+# key (for key, vm ) refers to the specific instance, example azurerm_virtual_machine.standard["vm1"] would be the fist instance
+# for is iterating through all keys in azurerm_virtual_machine.standard and returning the and returns (:) the key as a result
+# the key can now be accessed using [key]
+# vm (for key, vm) is the value of the key, azurerm_virtual_machine.standard["key"].value
+# 
 
 output "vm_details" {
-  value = { for key, vm in azurerm_virtual_machine.standard : key => {
+  value = { for key, vm in azurerm_virtual_machine.standard : key => {  
     name = vm.name
     pass = random_password.password[key].result
     }
