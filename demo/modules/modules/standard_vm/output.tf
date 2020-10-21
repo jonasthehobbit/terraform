c025644d159cd5e69f4b1c1eeb1cc05ebf7eeaab
Jamie Jonas <jamie@oneten.tech>
2020-10-21 16:24:27 +0100

    added comments

--- demo/modules/modules/standard_vm/output.tf
+++ demo/modules/modules/standard_vm/output.tf
@@ -1,14 +1,11 @@
 # The for expresstion below is detailed here
-# azurerm_virtual_machine.standard has multiple objects and we need an expression to combine the password and vmname
-# key (for key, vm ) refers to the specific instance, example azurerm_virtual_machine.standard["vm1"] would be the fist instance
-# for is iterating through all keys in azurerm_virtual_machine.standard and returning the and returns (:) the key as a result
-# the key can now be accessed using [key]
-# vm (for key, vm) is the value of the key, azurerm_virtual_machine.standard["key"].value
-# 
+# for each key and value in azurerm_virtual_machine.standard return the key and create a new object
+# with values name whcih will equak value.name 
+# and passw which equals random_password.password[key].result
 
 output "vm_details" {
-  value = { for key, vm in azurerm_virtual_machine.standard : key => {  
-    name = vm.name
+  value = { for key, value in azurerm_virtual_machine.standard : key => {  
+    name = value.name
     pass = random_password.password[key].result
     }
   }
